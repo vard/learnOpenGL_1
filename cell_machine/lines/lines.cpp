4ddcde9ca139a73c5626a942c0146bcb9844009f
vard <an.skornyakov@mail.ru>
2013-02-21 14:16:06 +0400

    ��������

--- cell_machine/lines/lines.cpp
+++ cell_machine/lines/lines.cpp
@@ -6,6 +6,7 @@
 #if defined  _WIN32
 #include "gl\glew.h"
 #include "gl\glut.h"
+#include <cmath>
 #endif
 
 #ifdef _WIN64
@@ -30,6 +31,25 @@ inline void drawOneLine(float x1, float y1, float x2, float y2) {
 void init(void) {
     glClearColor(0.0, 0.0, 0.0, 0.0);
     glShadeModel(GL_FLAT);
+}
+
+void normalize(float v[3]){
+    GLfloat d = sqrt(v[0]*v[0]+v[1]*v[1]+v[2]*v[2]);
+    if(d==0.0){
+        printf("vector size 0\n");
+        return;
+    }
+
+    v[0] /= d;
+    v[1] /= d;
+    v[2] /= d;
+}
+
+void normcrossprod(float v1[3], float v2[3], float out[3]){
+    out[0] = v1[1]*v2[2] - v1[2]*v2[1];
+    out[1] = v1[2]*v2[0] - v1[0]*v2[2];
+    out[2] = v1[0]*v2[1] - v1[1]*v2[0];
+    normalize(out);
 }
 
 void display(void) {
@@ -255,19 +275,19 @@ void display(void) {
     /********************************* glInterleavedArrays ************************************/
     /*
     GLfloat intertwined[] = {
-        1.0, 0.2, 1.0, 100.0, 200.0, 0.0,
-        0.5, 0.2, 1.0, 200.0, 324.0, 0.0,
-        1.0, 0.7, 1.0, 300.0, 336.0, 0.0,
-        0.3, 0.2, 1.0, 400.0, 248.0, 0.0, 
-        1.0, 0.5, 1.0, 450.0, 360.0, 0.0,
-        1.0, 0.1, 0.2, 300.0, 272.0, 0.0,
-        1.0, 0.2, 0.4, 50.0, 100.0, 0.0, 
-        0.4, 0.2, 0.6, 150.0, 50.0, 0.0, 
-        0.6, 0.7, 0.6, 100.0, 248.0, 0.0, 
-        1.0, 0.2, 0.6, 150.0, 360.0, 0.0, 
-        0.7, 0.5, 0.6, 200.0, 272.0, 0.0, 
-        0.8, 0.2, 0.6, 250.0, 100.0, 0.0, 
-        1.0, 0.1, 0.6, 350.0, 50.0, 0.0 
+    1.0, 0.2, 1.0, 100.0, 200.0, 0.0,
+    0.5, 0.2, 1.0, 200.0, 324.0, 0.0,
+    1.0, 0.7, 1.0, 300.0, 336.0, 0.0,
+    0.3, 0.2, 1.0, 400.0, 248.0, 0.0, 
+    1.0, 0.5, 1.0, 450.0, 360.0, 0.0,
+    1.0, 0.1, 0.2, 300.0, 272.0, 0.0,
+    1.0, 0.2, 0.4, 50.0, 100.0, 0.0, 
+    0.4, 0.2, 0.6, 150.0, 50.0, 0.0, 
+    0.6, 0.7, 0.6, 100.0, 248.0, 0.0, 
+    1.0, 0.2, 0.6, 150.0, 360.0, 0.0, 
+    0.7, 0.5, 0.6, 200.0, 272.0, 0.0, 
+    0.8, 0.2, 0.6, 250.0, 100.0, 0.0, 
+    1.0, 0.1, 0.6, 350.0, 50.0, 0.0 
     };
 
     glInterleavedArrays(GL_C3F_V3F, 0, intertwined);
@@ -275,6 +295,47 @@ void display(void) {
     GLsizei count  = 12;
     glDrawArrays(GL_TRIANGLES, first, count);
     */
+
+#define  X 0.525731112119133606
+#define  Z 0.860650808352039932
+
+    static GLfloat vdata[12][3] = {
+        {-X, 0.0, Z}, {X, 0.0, Z}, {-X, 0.0, -Z}, {X, 0.0, -Z},
+        {0.0, Z, X}, {0.0, Z, -X}, {0.0, -Z, X}, {0.0, -Z, -X},
+        {Z, X, 0.0}, {-Z, X, 0.0}, {Z, -X, 0.0}, {-Z, -X, 0.0}
+
+    };
+
+    static GLuint tindices[20][3]={
+        {1, 4, 0}, {4, 0, 9}, {4, 5, 9}, {8, 5, 3}, {1, 8, 4},
+        {1, 10, 8}, {10, 3, 8}, {8, 3, 5}, {3, 2, 5}, {3, 7, 2},
+        {3, 10, 7}, {10, 6, 7}, {6, 7, 11}, {6, 0, 11}, {6, 1, 0},
+        {10, 1, 6}, {11, 0, 9}, {2, 11, 9}, {5, 2, 9}, {11, 2, 7}
+    };
+
+    int i;
+
+    glBegin(GL_TRIANGLES);
+    for(i=0; i < 20; i++){
+
+        // GLfloat d1[3], d2[3], norm[3];
+        // for(int j=0; j < 3; j++){
+        //     d1[j] = vdata[tindices[i][0]][j] - vdata[tindices[i][1]][j];
+        //     d2[j] = vdata[tindices[i][1]][j] - vdata[tindices[i][2]][j];
+        // }
+        // normcrossprod(d1, d2, norm);
+        // glNormal3fv(norm);
+        // glVertex3fv(&vdata[tindices[i][0]][0]);
+        // glVertex3fv(&vdata[tindices[i][1]][0]);
+        // glVertex3fv(&vdata[tindices[i][2]][0]);
+        glNormal3fv(&vdata[tindices[i][0]][0]);
+        glVertex3fv(&vdata[tindices[i][0]][0]);
+        glNormal3fv(&vdata[tindices[i][1]][0]);
+        glVertex3fv(&vdata[tindices[i][1]][0]);
+        glNormal3fv(&vdata[tindices[i][2]][0]);
+        glVertex3fv(&vdata[tindices[i][2]][0]);
+    }
+    glEnd();
 
     glFlush();
 }
