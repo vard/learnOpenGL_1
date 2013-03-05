3c576992941504fe40507638c02fa7780fd1f7f6
vard <an.skornyakov@mail.ru>
2013-03-05 17:31:00 +0400

    + material_properties: added cutted sphere

--- cell_machine/material_properties/material_properties.cpp
+++ cell_machine/material_properties/material_properties.cpp
@@ -22,7 +22,11 @@
 
 
 void init(void){
+    GLfloat scene_ambient[] = {0.2, 0.2, 0.2, 1.0};
     glClearColor(0.0, 0.0, 0.0, 0.0);
+    // Общее фоновое освещение
+    glLightModelfv(GL_LIGHT_MODEL_AMBIENT, scene_ambient);
+    glLightModelf(GL_LIGHT_MODEL_TWO_SIDE, GL_TRUE);
     glShadeModel(GL_SMOOTH);
     glEnable(GL_LIGHTING);
     glEnable(GL_LIGHT0);
@@ -30,8 +34,11 @@ void init(void){
 }
 
 void display(void){
+    GLdouble eqn[4] = {+1.0, 0.0, -1.0, +2.25}; // plane 2.8x + z - 6.3 = 0
     GLfloat no_mat[] = {0.0, 0.0, 0.0, 1.0};
+    GLfloat mat_back[] = {0.1, 0.9, 0.1, 1.0};
     GLfloat mat_ambient[] = {0.7, 0.7, 0.7, 1.0};
+    GLfloat mat_ambient_back[] = {0.1, 0.1, 0.7, 1.0};
     GLfloat mat_ambient_color[] = {0.8, 0.8, 0.2, 1.0};
     GLfloat mat_diffuse[] = {1.0, 0.5, 0.8, 1.0};
     GLfloat mat_specular[] = {1.0, 1.0, 1.0, 1.0};
@@ -40,6 +47,28 @@ void display(void){
     GLfloat high_shininess[] = {100.0};
     GLfloat mat_emission[] = {0.3, 0.2, 0.2, 0.0};
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+    // Срежем часть сферы
+    glClipPlane(GL_CLIP_PLANE0, eqn);
+    glEnable(GL_CLIP_PLANE0);
+
+    glPushMatrix();
+    glTranslatef(1.25, -3.0, 0.0);
+    glMaterialfv(GL_FRONT, GL_AMBIENT, no_mat);          // фоновый цвет материала
+    glMaterialfv(GL_FRONT, GL_DIFFUSE, mat_diffuse);     // Рассеянный цвет материала
+    glMaterialfv(GL_FRONT, GL_SPECULAR, mat_specular);   // Отраженный цвет материала
+    glMaterialfv(GL_FRONT, GL_SHININESS, high_shininess);// Коэффициент зеркального отражения
+    glMaterialfv(GL_FRONT, GL_EMISSION, no_mat);         // Излучаемый цвет материала
+
+    glMaterialfv(GL_BACK, GL_AMBIENT, mat_back);         // фоновый цвет материала
+    glMaterialfv(GL_BACK, GL_DIFFUSE, mat_ambient_back); // Рассеянный цвет материала
+    glMaterialfv(GL_BACK, GL_SPECULAR, mat_specular);    // Отраженный цвет материала
+    glMaterialfv(GL_BACK, GL_SHININESS, high_shininess); // Коэффициент зеркального отражения
+    glMaterialfv(GL_BACK, GL_EMISSION, no_mat);          // Излучаемый цвет материала
+    glutSolidSphere(3.5, 16, 16);
+    glPopMatrix();
+    glDisable(GL_CLIP_PLANE0);
+
 
     // Сфера с рассеянным диффузным цветом, без фонового или зеркального цвета
     glPushMatrix();
@@ -87,6 +116,7 @@ void display(void){
     glColor3f(1.0, 1.0, 1.0);
     glutSolidSphere(1.0, 16, 16);
     glPopMatrix();
+   
 
     glFlush();
 }
